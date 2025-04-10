5678bbb42277d1ac3e6224d8ca9e982d23178c08
Nefertaum <jakubbrunner@gmai.com>
2025-04-10 14:22:25 +0200

    Add contacts.html, finish styling, try js

---
+++ resources/script.js
@@ -0,0 +1,8 @@
+function makeItDark (event) {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+ }
+ 
+ document.getElementById("dark").addEventListener("click", makeItDark);
+ 
+console.log("I'm here");
